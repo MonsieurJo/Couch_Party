23aedcc0494b5fddc807805318f20c81c88e284f
MSI\Lucas <lucasblon@orange.fr>
2017-09-25 17:19:57 +0200

    rigidbody addforce

--- Assets/Scripts/ScriptsPop/PopItems.cs
+++ Assets/Scripts/ScriptsPop/PopItems.cs
@@ -11,34 +11,27 @@ public class PopItems : MonoBehaviour {
 
     private float xRandom;
     private float zRandom;
-    public Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating("RandoPop", 0.001f, spawnInterval);
-
-		xRandom = UnityEngine.Random.Range(-1,1);
-		zRandom = UnityEngine.Random.Range(-1,1);
 	}
 	
-	// Update is called once per frame
-	void FixeUpdate () {
-
-		rb.AddForce(xRandom, 0, zRandom, ForceMode.Force);
-
-	}
 
 	void RandoPop () {
 
 		if (nbmaxprefab < 100) {
 
   			int tempory = UnityEngine.Random.Range(0,popRandom.Count);
-  			Instantiate(popRandom[tempory], transform.position, transform.rotation);
+  			GameObject iGo;
+  			iGo = Instantiate(popRandom[tempory], transform.position, transform.rotation);
+  			xRandom = UnityEngine.Random.Range(-4,4);
+			zRandom = UnityEngine.Random.Range(-4,4);
+  			iGo.GetComponent<Rigidbody>().AddForce(xRandom, 0, zRandom, ForceMode.Impulse);
         	nbmaxprefab += 1f;
 
 		}
 
 	}
-
 
 }
