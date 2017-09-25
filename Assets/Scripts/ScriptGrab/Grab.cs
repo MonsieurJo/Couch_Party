5dffd66dfb7db0fbb0558b4d6491a2c4b0f0af8d
PCP-TRISTAN\trist <tristanlavaud@hotmail.fr>
2017-09-25 15:40:36 +0200

    Début du grab et ca me casse déjà les roubignolles !

---
+++ Assets/Scripts/ScriptGrab/Grab.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections ;
+using System.Collections.Generic ;
+using UnityEngine ;
+
+public class Grab : MonoBehaviour 
+{
+	public Transform player ;
+	public Transform playerCam ;
+	public float throwForce = 10 ;
+	private bool hasPlayer = false ;
+	private bool beingCarried = false ;
+	public AudioClip[] soundtoPlay ;
+	public int dmg ;
+	private bool touched = false ;
+
+
+
+void Update()
+{
+	float dist = Vector3.Distance(gameObject.transform.position, player.position) ;
+	if (dist <= 2.5f)
+	{
+		hasPlayer = true;
+	}
+	else 
+	{
+		hasPlayer = false;
+	}
+	if (hasPlayer && Input.GetButtonDown("use"))
+	{
+		GetComponent<Rigidbody>().isKinematic = true ;
+		transform.parent = playerCam ;
+		beingCarried = true ;
+	}
+	if (beingCarried)
+	{
+		if (touched)
+		{
+			GetComponent<Rigidbody>().isKinematic = false ;
+			transform.parent = null ;
+			beingCarried = false ;
+			touched = false ;
+		}
+		if (Input.GetMouseButtonDown(0))
+		{
+			GetComponent<Rigidbody>().isKinematic = false ;
+			transform.parent = null ; 
+			beingCarried = false ;
+			GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce) ;
+		}
+		else if (Input.GetMouseButtonDown(1))
+		{
+			GetComponent<Rigidbody>().isKinematic = false ;
+				transform.parent = null ;
+			beingCarried = false ;;
+		}
+	}
+}
+
+void RandomAudio()
+{
+	if (GetComponent<AudioSource>().isPlaying)
+	{
+		GetComponent<AudioSource>().clip = soundtoPlay[Random.Range(0, soundtoPlay.Length)] ;
+		GetComponent<AudioSource>().Play() ;
+	}
+}
+
+void OnTriggerEnter()
+{
+	if (beingCarried)
+	{
+		touched = true;
+	}
+}
+}
