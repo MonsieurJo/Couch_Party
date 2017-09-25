0db097da5ec961d7d512683b106d3e3970546a7e
MSI\Lucas <lucasblon@orange.fr>
2017-09-25 14:34:47 +0200

    pop items scene lucas appuyez sur f

---
+++ Assets/Scripts/ApplicationItems.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationItems : MonoBehaviour {
+
+	public ScriptableItems my_scriptable;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Assets/Scripts/ScriptableItems.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( menuName ="scriptablemitems")]
+
+public class ScriptableItems : ScriptableObject {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
