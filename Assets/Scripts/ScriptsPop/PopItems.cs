ecfdf2ecf7e66c309d4585bb09168990582c1768
MSI\Lucas <lucasblon@orange.fr>
2017-09-25 16:12:51 +0200

    Pop des items aléatoires

---
+++ Assets/Scripts/ScriptsPop/PopItems.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PopItems : MonoBehaviour {
+
+    public List<GameObject> popRandom = new List<GameObject>();
+    private float nbmaxprefab =0f;
+    public float spawnInterval = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("RandoPop", 0.001f, spawnInterval);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void RandoPop () {
+
+		if (nbmaxprefab < 100) {
+
+  			int tempory = UnityEngine.Random.Range(0,popRandom.Count);
+  			Instantiate(popRandom[tempory], transform.position, transform.rotation);
+        	nbmaxprefab += 1f;
+
+		}
+
+	}
+
+}
