cc1f04c84a52de229092b3ecd4d42a747cfea426
Jordan <j.drespincho@yahoo.com>
2017-09-25 13:19:23 +0200

    Creation du script Menu Manager

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject lobby;
+	public GameObject startMenu;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayOnClick(){
+		startMenu.SetActive(false);
+		lobby.SetActive(true);
+	}
+
+	public void QuitOnClick(){
+		Application.Quit();
+		Debug.Log("Quit");
+	}
+}
