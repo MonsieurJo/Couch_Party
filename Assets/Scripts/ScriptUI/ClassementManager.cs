688470d4503409670f7213b0338b69b2f8b9cae3
Jordan <j.drespincho@yahoo.com>
2017-09-29 10:33:10 +0200

    Update Classement Manager

--- Assets/Scripts/ScriptUI/ClassementManager.cs
+++ Assets/Scripts/ScriptUI/ClassementManager.cs
@@ -6,80 +6,91 @@ using UnityEngine.UI;
 public class ClassementManager : MonoBehaviour {
 
 
-// 	private int scoreJ1;
-// 	private int scoreJ2;
-// 	private int scoreJ3;
-// 	private int scoreJ4;
+	private int scoreJ1;
+	private int scoreJ2;
+	private int scoreJ3;
+	private int scoreJ4;
 
-// 	public Text firstPlayer;
-// 	public Text secondPlayer;
-// 	public Text thirdPlayer;
-// 	public Text fourthPlayer;
+	public Text firstPlayer;
+	public Text secondPlayer;
+	public Text thirdPlayer;
+	public Text fourthPlayer;
 
-// 	public GameObject uiManagerIG;
+	public GameObject uiManagerIG;
 
-// 	// Use this for initialization
-// 	void Start () {
-// 		scoreJ1 = uiManagerIG.GetComponent<UIManagerIG>().DisplayScore(1);
-// 		scoreJ2 = uiManagerIG.GetComponent<UIManagerIG>().DisplayScore(2);
-// 		scoreJ3 = uiManagerIG.GetComponent<UIManagerIG>().DisplayScore(3);
-// 		scoreJ4 = uiManagerIG.GetComponent<UIManagerIG>().DisplayScore(4);
+	public enum Scores {
+		Joueur1,
+		Joueur2,
+		Joueur3,
+		Joueur4,
+		numEntries
+	}
 
-// 		if (scoreJ1 > scoreJ2 && scoreJ2 > scoreJ3 && scoreJ3 > scoreJ4){
-// 			firstPlayer.text = "Joueur 1 : "+scoreJ1+" points ";
-// 			secondPlayer.text = "Joueur 2 : "+scoreJ2+" points ";
-// 			thirdPlayer.text = "Joueur 3 : "+scoreJ3+" points ";
-// 			fourthPlayer.text = "Joueur 4 : "+scoreJ4+" points ";
-// 		} else if (scoreJ1 > scoreJ2 && scoreJ2 > scoreJ3 && scoreJ3 > scoreJ4){
-// 			firstPlayer.text = "Joueur 1 : "+scoreJ1+" points ";
-// 			secondPlayer.text = "Joueur 2 : "+scoreJ2+" points ";
-// 			thirdPlayer.text = "Joueur 3 : "+scoreJ3+" points ";
-// 			fourthPlayer.text = "Joueur 4 : "+scoreJ4+" points ";
-// 		} else if (scoreJ1 > scoreJ2 && scoreJ2 > scoreJ3 && scoreJ3 > scoreJ4){
-// 			firstPlayer.text = "Joueur 1 : "+scoreJ1+" points ";
-// 			secondPlayer.text = "Joueur 2 : "+scoreJ2+" points ";
-// 			thirdPlayer.text = "Joueur 3 : "+scoreJ3+" points ";
-// 			fourthPlayer.text = "Joueur 4 : "+scoreJ4+" points ";
-// 		} else if (scoreJ1 > scoreJ2 && scoreJ2 > scoreJ3 && scoreJ3 > scoreJ4){
-// 			firstPlayer.text = "Joueur 1 : "+scoreJ1+" points ";
-// 			secondPlayer.text = "Joueur 2 : "+scoreJ2+" points ";
-// 			thirdPlayer.text = "Joueur 3 : "+scoreJ3+" points ";
-// 			fourthPlayer.text = "Joueur 4 : "+scoreJ4+" points ";
-// 		} else if (scoreJ1 > scoreJ2 && scoreJ2 > scoreJ3 && scoreJ3 > scoreJ4){
-// 			firstPlayer.text = "Joueur 1 : "+scoreJ1+" points ";
-// 			secondPlayer.text = "Joueur 2 : "+scoreJ2+" points ";
-// 			thirdPlayer.text = "Joueur 3 : "+scoreJ3+" points ";
-// 			fourthPlayer.text = "Joueur 4 : "+scoreJ4+" points ";
-// 		} else if (scoreJ1 > scoreJ2 && scoreJ2 > scoreJ3 && scoreJ3 > scoreJ4){
-// 			firstPlayer.text = "Joueur 1 : "+scoreJ1+" points ";
-// 			secondPlayer.text = "Joueur 2 : "+scoreJ2+" points ";
-// 			thirdPlayer.text = "Joueur 3 : "+scoreJ3+" points ";
-// 			fourthPlayer.text = "Joueur 4 : "+scoreJ4+" points ";
-// 		} else if (scoreJ1 > scoreJ2 && scoreJ2 > scoreJ3 && scoreJ3 > scoreJ4){
-// 			firstPlayer.text = "Joueur 1 : "+scoreJ1+" points ";
-// 			secondPlayer.text = "Joueur 2 : "+scoreJ2+" points ";
-// 			thirdPlayer.text = "Joueur 3 : "+scoreJ3+" points ";
-// 			fourthPlayer.text = "Joueur 4 : "+scoreJ4+" points ";
-// 		} else if (scoreJ1 > scoreJ2 && scoreJ2 > scoreJ3 && scoreJ3 > scoreJ4){
-// 			firstPlayer.text = "Joueur 1 : "+scoreJ1+" points ";
-// 			secondPlayer.text = "Joueur 2 : "+scoreJ2+" points ";
-// 			thirdPlayer.text = "Joueur 3 : "+scoreJ3+" points ";
-// 			fourthPlayer.text = "Joueur 4 : "+scoreJ4+" points ";
-// 		} else if (scoreJ1 > scoreJ2 && scoreJ2 > scoreJ3 && scoreJ3 > scoreJ4){
-// 			firstPlayer.text = "Joueur 1 : "+scoreJ1+" points ";
-// 			secondPlayer.text = "Joueur 2 : "+scoreJ2+" points ";
-// 			thirdPlayer.text = "Joueur 3 : "+scoreJ3+" points ";
-// 			fourthPlayer.text = "Joueur 4 : "+scoreJ4+" points ";
-// 		} else if (scoreJ1 > scoreJ2 && scoreJ2 > scoreJ3 && scoreJ3 > scoreJ4){
-// 			firstPlayer.text = "Joueur 1 : "+scoreJ1+" points ";
-// 			secondPlayer.text = "Joueur 2 : "+scoreJ2+" points ";
-// 			thirdPlayer.text = "Joueur 3 : "+scoreJ3+" points ";
-// 			fourthPlayer.text = "Joueur 4 : "+scoreJ4+" points ";
-// 		]
-// 	}
-	
-// 	// Update is called once per frame
-// 	void Update () {
-		
-// 	}
+	public int[] score;
+
+	void Awake(){
+		score = new int[(int)Scores.numEntries];
+		score[(int)Scores.Joueur1]= uiManagerIG.GetComponent<UIManagerIG>().DisplayScore(1);
+		score[(int)Scores.Joueur2]= uiManagerIG.GetComponent<UIManagerIG>().DisplayScore(2);
+		score[(int)Scores.Joueur3]= uiManagerIG.GetComponent<UIManagerIG>().DisplayScore(3);
+		score[(int)Scores.Joueur4]= uiManagerIG.GetComponent<UIManagerIG>().DisplayScore(4);
+		int maxIndex=-1;
+		int maxValue = 0;
+		for(int i=0;i<(int)Scores.numEntries;i++){
+			if(score[i]>maxValue){
+				maxValue=score[i];
+				maxIndex=i;
+			}
+		}
+
+		if(maxIndex >=0){
+			Debug.Log((Scores)maxIndex + "   " + maxValue);
+			firstPlayer.text = (Scores)maxIndex + " : "+ maxValue+" points ";
+		}
+		int secondIndex=-1;
+		int secondValue = 0;
+		for(int i=0;i<(int)Scores.numEntries;i++){
+			if (score[i] == maxValue){
+				i++;
+			}
+			if(score[i]>secondValue){
+				secondValue=score[i];
+				secondIndex=i;
+			}
+		}
+		if(secondIndex >=0){
+			Debug.Log((Scores)secondIndex + "   " + secondValue);
+			secondPlayer.text = (Scores)secondIndex + " : " + secondValue + " points ";
+		}
+		int thirdIndex=-1;
+		int thirdValue = 0;
+		for(int i=0;i<(int)Scores.numEntries;i++){
+			if (score[i] == maxValue || score[i] == secondValue){
+				i++;
+			}
+			if(score[i]>thirdValue){
+				thirdValue=score[i];
+				thirdIndex=i;
+			}
+		}
+		if(thirdIndex >=0){
+			Debug.Log((Scores)thirdIndex + "   " + thirdValue);
+			thirdPlayer.text = (Scores)thirdIndex + " : " + thirdValue + " points ";
+		}
+		int fourthIndex=-1;
+		int fourthValue = 0;
+		for(int i=0;i<(int)Scores.numEntries;i++){
+			if (score[i] == maxValue || score[i] == secondValue || score[i] == thirdValue){
+				i++;
+			}
+			if(score[i]>fourthValue){
+				fourthValue=score[i];
+				fourthIndex=i;
+			}
+		}
+		if(fourthIndex >=0){
+			Debug.Log((Scores)fourthIndex + "   " + fourthValue);
+			fourthPlayer.text = (Scores)fourthIndex + " : " + fourthValue + " points ";
+		}
+	}
 }
